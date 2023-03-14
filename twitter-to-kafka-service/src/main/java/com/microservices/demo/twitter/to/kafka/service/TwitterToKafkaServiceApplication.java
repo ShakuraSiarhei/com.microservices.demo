ea822226a48234eb9d7863317efffe21a5f594e2
Siarhei Shakura <s.shakura@andersenlab.com>
2023-03-14 10:48:35 +0100

    Added TwitterKafkaStreamRunner

--- twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -1,6 +1,7 @@
 package com.microservices.demo.twitter.to.kafka.service;
 
 import com.microservices.demo.twitter.to.kafka.service.config.TwitterToKafkaServiceConfigData;
+import com.microservices.demo.twitter.to.kafka.service.runner.StreamRunner;
 import java.util.Arrays;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
@@ -15,6 +16,7 @@ public class TwitterToKafkaServiceApplication implements CommandLineRunner {
 
   private static final Logger LOG = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class.getName());
   private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+  private final StreamRunner streamRunner;
 
   public static void main(String[] args) {
     SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
@@ -25,5 +27,6 @@ public class TwitterToKafkaServiceApplication implements CommandLineRunner {
     LOG.info("App starts...");
     LOG.info(Arrays.toString(twitterToKafkaServiceConfigData.getTwitterKeywords().toArray()));
     LOG.info(twitterToKafkaServiceConfigData.getWelcomeMessage());
+    streamRunner.start();
   }
 }
